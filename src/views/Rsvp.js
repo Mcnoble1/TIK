bdf42d1befea81d850124ac33d2eb8ae8f582ebb
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-08-27 01:38:34 +0100

    feat: updated rsvp component :octocat:

--- src/views/Rsvp.js
+++ src/views/Rsvp.js
@@ -1,6 +1,5 @@
 import React from "react";
-// javascript plugin used to create scrollbars on windows
-// import PerfectScrollbar from "perfect-scrollbar";
+
 import MakeRsvp from "./MakeRsvp";
 // reactstrap components
 import {
@@ -11,31 +10,8 @@ import {
 // core components
 import Navbar from "components/Navbar/Navbar.js";
 
-
-// let ps = null;
-
 export default function ProfilePage() {
-//   const [tabs, setTabs] = React.useState(1);
-//   React.useEffect(() => {
-//     if (navigator.platform.indexOf("Win") > -1) {
-//       document.documentElement.className += " perfect-scrollbar-on";
-//       document.documentElement.classList.remove("perfect-scrollbar-off");
-//       let tables = document.querySelectorAll(".table-responsive");
-//       for (let i = 0; i < tables.length; i++) {
-//         ps = new PerfectScrollbar(tables[i]);
-//       }
-//     }
-//     document.body.classList.toggle("profile-page");
-//     // Specify how to clean up after this effect:
-//     return function cleanup() {
-//       if (navigator.platform.indexOf("Win") > -1) {
-//         ps.destroy();
-//         document.documentElement.className += " perfect-scrollbar-off";
-//         document.documentElement.classList.remove("perfect-scrollbar-on");
-//       }
-//       document.body.classList.toggle("profile-page");
-//     };
-//   }, []);
+
   return (
     <>
       <Navbar />   
