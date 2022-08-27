c4f39ad2ed2bd4e2afe25733da38b03145dd7e16
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-08-27 01:39:54 +0100

    feat: updated dashboard component :octocat:

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,6 +1,5 @@
 import React from "react";
-// javascript plugin used to create scrollbars on windows
-// import PerfectScrollbar from "perfect-scrollbar";
+
 import CreateEvent from "./CreateEvent";
 // reactstrap components
 import {
@@ -14,27 +13,7 @@ import Navbar from "components/Navbar/Navbar.js";
 // let ps = null;
 
 export default function ProfilePage() {
-  // const [tabs, setTabs] = React.useState(1);
-  // React.useEffect(() => {
-  //   if (navigator.platform.indexOf("Win") > -1) {
-  //     document.documentElement.className += " perfect-scrollbar-on";
-  //     document.documentElement.classList.remove("perfect-scrollbar-off");
-  //     let tables = document.querySelectorAll(".table-responsive");
-  //     for (let i = 0; i < tables.length; i++) {
-  //       ps = new PerfectScrollbar(tables[i]);
-  //     }
-  //   }
-  //   document.body.classList.toggle("profile-page");
-  //   // Specify how to clean up after this effect:
-  //   return function cleanup() {
-  //     if (navigator.platform.indexOf("Win") > -1) {
-  //       ps.destroy();
-  //       document.documentElement.className += " perfect-scrollbar-off";
-  //       document.documentElement.classList.remove("perfect-scrollbar-on");
-  //     }
-  //     document.body.classList.toggle("profile-page");
-  //   };
-  // }, []);
+  
   return (
     <>
       <Navbar />   
